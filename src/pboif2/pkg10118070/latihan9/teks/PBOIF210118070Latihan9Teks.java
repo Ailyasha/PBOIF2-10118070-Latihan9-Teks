6616e2e0c1700c5f9bb9285e9b9381a3a16c788e
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-09-25 22:35:51 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan9/teks/PBOIF210118070Latihan9Teks.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ */
+public class PBOIF210118070Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
